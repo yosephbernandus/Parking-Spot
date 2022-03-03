00726edbe0469a780d0476633dfe72a0a304977c
Yosephbernandus <yosephbernandus@gmail.com>
2022-03-03 12:57:19 +0700

    Simple setup map screen google map

--- app/src/main/java/mobile/android/parkingspotfinder/presentation/MapScreen.kt
+++ app/src/main/java/mobile/android/parkingspotfinder/presentation/MapScreen.kt
@@ -1,9 +1,49 @@
 package mobile.android.parkingspotfinder.presentation
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ToggleOff
+import androidx.compose.material.icons.filled.ToggleOn
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapUiSettings
 
 @Composable
 fun MapScreen(
     viewModel: MapsViewModel = androidx.lifecycle.viewmodel.compose.viewModel()
 ) {
+    val scaffoldState = rememberScaffoldState()
+    val uiSettings = remember {
+        MapUiSettings(zoomControlsEnabled = false)
+    }
+    Scaffold(
+        scaffoldState = scaffoldState,
+        floatingActionButton = {
+            FloatingActionButton(onClick = {
+                /*TODO*/
+            }) {
+                Icon(
+                    ImageVector = if (viewModel.state.isFallOutMap)
+                    {
+                        Icons.Default.ToggleOff
+                    } else Icons.Default.ToggleOn,
+                    contentDescription = "Toggle Fallout map"
+                )
+            }
+        }
+    ) {
+        GoogleMap(
+            modifier = Modifier.fillMaxSize(),
+            properties = viewModel.state.properties,
+            uiSettings = uiSettings,
+            onMapLongClick = {
 
+            }
+        )
+    }
 }
